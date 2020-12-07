9b0a2b4f73526b0dcca1bd53595354497508d62e
daisydex <daisy.congiu@gmail.com>
2020-12-07 19:41:10 +0100

    Capitalized li

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute(
     "src",
