56aa510c500c556d5a598038caa6bd91f62da5a3
daisydex <daisy.congiu@gmail.com>
2020-12-08 18:46:20 +0100

    Fetched real data for 1st forecast

--- src/index.js
+++ src/index.js
@@ -40,8 +40,12 @@ function formatDate(date) {
   ${hours}:${minutes}`;
 }
 
+function formatHours(timestamp) {
+  return `${hours}: ${minutes}`;
+
+}
 function displayWeatherCondition(response) {
-  console.log(response);
+  
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
 
@@ -58,11 +62,30 @@ function displayWeatherCondition(response) {
   );
   
 }
+function displayForecast(response) {
+console.log(response);
+
+let forecastElement= document.querySelector("#forecast");
+let forecast=response.data.list[0];
+
+forecastElement.innerHTML= `
+<div class="col-2">
+<h3>${formatHours(forecast.dt * 1000)}</h3>
+<img src="http://openweathermap.org/img/wn/${forecast.data.weather[0].icon}@2x.png"/>
+<div class="weather-forecast-temperature">
+<strong>${Math.round(forecast.main.temp_max)}°C</strong> ${Math.round(forecast.main.temp_min)}°C
+  </div>
+    </div>`
+
+}
 
 function searchCity(city) {
   let apiKey = "cc3c1c3a90bf11d5dba4d1d793f72cde";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -71,11 +94,14 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
+
 function searchLocation(position) {
   event.preventDefault();
   let apiKey = "cc3c1c3a90bf11d5dba4d1d793f72cde";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  
 }
 
 function getCurrentLocation(event) {
